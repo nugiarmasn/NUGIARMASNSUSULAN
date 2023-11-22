6286fda7b0cc3b8d6430e950c67bb22e8c3b4f9b
nugiarmasnurrizky <nugiarmasnurizky@gmail.com>
2023-11-22 16:48:36 +0700

    frist push

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String kata = "PERTAMA";
+        Map<Character, Integer> hasil = hitungKarakter(kata);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("String: " + kata);
+        System.out.println("Hasil: " + hasil);
+    }
+
+
+    private static Map<Character, Integer> hitungKarakter(String input) {
+
+        Map<Character, Integer> karakterCount = new HashMap<>();
+
+
+        for (char karakter : input.toCharArray()) {
+
+            if (karakterCount.containsKey(karakter)) {
+
+                karakterCount.put(karakter, karakterCount.get(karakter) + 1);
+            } else {
+
+                karakterCount.put(karakter, 1);
+            }
         }
+
+
+        return karakterCount;
     }
 }
